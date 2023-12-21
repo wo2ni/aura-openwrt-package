e4c5b44aff54fd089ae4cb5e2f73484f14240464
wo2ni <i_o_i_o@163.com>
2023-12-21 21:11:28 +0800

    rm

--- luci-app-tencentddns/files/luci/controller/tencentddns.lua
+++
@@ -1,5 +0,0 @@
-module("luci.controller.tencentddns",package.seeall)
-function index()
-entry({"admin", "services"}, firstchild(), "腾讯云设置", 30).dependent=false
-entry({"admin", "services", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),2)
-end
